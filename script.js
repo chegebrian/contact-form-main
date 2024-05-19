4de53c08cf9fba8be74f32e31716944cb72dc8eb
Brian Waithaka <waithakabrian8@gmail.com>
2024-05-19 08:50:34 +0300

    implement error state

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const messageEl = document.querySelector("#message");
 const enquiryEl = document.querySelector("#enquiry");
 const supportEl = document.querySelector("#support");
 const checkboxEl = document.querySelector("#checkbox");
+const radiobuttonsEl = document.querySelectorAll("input[name=radio]");
 
 //error element
 const fnameErr = document.querySelector(".fname-err");
@@ -14,6 +15,9 @@ const emailErr = document.querySelector(".email-err");
 const radioErr = document.querySelector(".radio-err");
 const textboxErr = document.querySelector(".textbox-err");
 const checkboxErr = document.querySelector(".checkbox-err");
+
+//radio section
+const radioEl = document.querySelector(".radio");
 
 //form element
 const formEl = document.querySelector("form");
@@ -26,24 +30,69 @@ formEl.addEventListener("submit", (e) => {
   }
 });
 
+firstNameEl.addEventListener("input", (e) => {
+  if (firstNameEl.validity.valid) {
+    fnameErr.textContent = "";
+  } else showError();
+});
+lastNameEl.addEventListener("input", (e) => {
+  if (lastNameEl.validity.valid) {
+    lnameErr.textContent = "";
+  } else showError();
+});
+emailEl.addEventListener("input", (e) => {
+  if (emailEl.validity.valid) {
+    emailErr.textContent = "";
+  } else showError();
+});
+messageEl.addEventListener("input", (e) => {
+  if (messageEl.validity.valid) {
+    textboxErr.textContent = "";
+  } else showError();
+});
+
+radiobuttonsEl.forEach((radio) => {
+  radio.addEventListener("input", () => {
+    if (radio.checked) {
+      radioErr.textContent = "";
+    } else showError();
+  });
+});
+
+checkboxEl.addEventListener("input", (e) => {
+  if (checkboxEl.checked) {
+    checkboxErr.textContent = "";
+  } else showError();
+});
+
 function showError() {
   if (firstNameEl.validity.valueMissing) {
     fnameErr.textContent = "input required";
   }
+  if (firstNameEl.validity.patternMismatch) {
+    fnameErr.textContent = "invalid name";
+  }
   if (lastNameEl.validity.valueMissing) {
     lnameErr.textContent = "input required";
+  }
+  if (lastNameEl.validity.patternMismatch) {
+    lnameErr.textContent = "invalid name";
   }
   if (emailEl.validity.valueMissing) {
     emailErr.textContent = "input required";
   }
-  if (!enquiryEl.checked || !supportEl.checked) {
+  if (emailEl.validity.typeMismatch) {
+    emailErr.textContent = "invalid email address";
+  }
+  if (!enquiryEl.checked && !supportEl.checked) {
     radioErr.textContent = "Please select a query type";
-  } else radioErr.textContent = "";
+  }
 
   if (messageEl.validity.valueMissing) {
     textboxErr.textContent = "input required";
   }
-  if(!checkboxEl.checked) {
-    checkboxErr.textContent = "To submit this form, please consent to being contacted";
-  }else checkboxEl.textContent = "";
+  if (!checkboxEl.checked) {
+    checkboxErr.textContent =
+      "To submit this form, please consent to being contacted";
+  }
 }
