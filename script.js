37be37872b4cd5910bf252a0aa21c42144c3f697
Brian Waithaka <waithakabrian8@gmail.com>
2024-05-20 05:13:51 +0300

    feat - implement error-state

--- script.js
+++ script.js
@@ -33,21 +33,25 @@ formEl.addEventListener("submit", (e) => {
 firstNameEl.addEventListener("input", (e) => {
   if (firstNameEl.validity.valid) {
     fnameErr.textContent = "";
+    firstNameEl.style.borderColor = "";
   } else showError();
 });
 lastNameEl.addEventListener("input", (e) => {
   if (lastNameEl.validity.valid) {
     lnameErr.textContent = "";
+    lastNameEl.style.borderColor = "";
   } else showError();
 });
 emailEl.addEventListener("input", (e) => {
   if (emailEl.validity.valid) {
     emailErr.textContent = "";
+    emailEl.style.borderColor = "";
   } else showError();
 });
 messageEl.addEventListener("input", (e) => {
   if (messageEl.validity.valid) {
     textboxErr.textContent = "";
+    messageEl.style.borderColor = "";
   } else showError();
 });
 
@@ -68,21 +72,27 @@ checkboxEl.addEventListener("input", (e) => {
 function showError() {
   if (firstNameEl.validity.valueMissing) {
     fnameErr.textContent = "input required";
+    firstNameEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (firstNameEl.validity.patternMismatch) {
     fnameErr.textContent = "invalid name";
+    firstNameEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (lastNameEl.validity.valueMissing) {
     lnameErr.textContent = "input required";
+    lastNameEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (lastNameEl.validity.patternMismatch) {
     lnameErr.textContent = "invalid name";
+    lastNameEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (emailEl.validity.valueMissing) {
     emailErr.textContent = "input required";
+    emailEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (emailEl.validity.typeMismatch) {
     emailErr.textContent = "invalid email address";
+    emailEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (!enquiryEl.checked && !supportEl.checked) {
     radioErr.textContent = "Please select a query type";
@@ -90,6 +100,7 @@ function showError() {
 
   if (messageEl.validity.valueMissing) {
     textboxErr.textContent = "input required";
+    messageEl.style.borderColor = "hsl(0, 66%, 56%)";
   }
   if (!checkboxEl.checked) {
     checkboxErr.textContent =
